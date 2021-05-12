6b23437fe72b9f04c0a29be188a80fa216d97019
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-12 10:34:45 +0200

    index bingo

--- Precourse/Bingo/index.js
+++ Precourse/Bingo/index.js
@@ -1,3 +1,12 @@
 const bingo = () => {
-    const userName = prompt("Introduce your name", "John Doe");
+    const userName = prompt("Introduce your name", "Your Name");
+    const numbers = [];
+
+    while (numbers.length < 15) {
+        let r = Math.floor(Math.random() * 15) + 1;
+        if (numbers.indexOf(r) === -1) numbers.push(r);
+    }
+    console.table(numbers);
 };
+
+bingo();
