b54711360857008201c0e36e8cb58af0b143c2ed
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-18 12:30:58 +0200

    bingo function added

--- Precourse/Bingo/index.js
+++ Precourse/Bingo/index.js
@@ -10,6 +10,7 @@ const listOfNumbers = () => {
     console.log(randomNumbers);
 };
 
+// Constructor de columnas
 function columns(c0, c1, c2, c3, c4) {
     this.column0 = c0;
     this.column1 = c1;
@@ -18,7 +19,7 @@ function columns(c0, c1, c2, c3, c4) {
     this.column4 = c4;
 }
 
-// Función que genera el cartón
+// Función que genera el cartón a través de pasarle datos al constructor
 function row(r0, r1, r2) {
     // console.clear();
     let row0 = new columns(r0[0], r0[1], r0[2], r0[3], r0[4]);
@@ -42,37 +43,19 @@ const cardboardNumbers = () => {
 //Creación del cartón
 const bingoCardBoard = () => {
     auxArray = cardboardNumbers();
-    console.log(auxArray);
     let r0 = auxArray.slice(0, 5);
     let r1 = auxArray.slice(5, 10);
     let r2 = auxArray.slice(10);
     row(r0, r1, r2);
 };
 
-bingoCardBoard();
+// Programa principal
+const bingo = () => {
+    const userName = prompt("Introduce your name", "Your Name");
+    bingoCardBoard();
+};
 
-// const bingo = () => {
-//     // const userName = prompt("Introduce your name", "Your Name");
-//     const auxArray = [];
-//     const objects = {};
-
-//     function RandomNumber(number, matched) {
-//         this.number = number;
-//         this.matched = matched;
-//     }
-
-//     const testObject = new RandomNumber(1, false);
-//     const testObject2 = new RandomNumber(3, false);
-//     console.log(testObject);
-
-// Genera un número aleatorio del 1 al 99 y tras comprobar que no exista en auxArray, lo introduce en el mismo hasta que este tenga 15 elementos
-// También introduce un objeto en el array objects con dos key values, el número (r) y el boolean del estado matched
-
-// const bingoCard = {
-//     row1: objects.slice(0, 5),
-//     row2: objects.slice(5, 10),
-//     row3: objects.slice(10),
-// };
+bingo();
 
 // console.log(auxArray);
 // console.table(objects);
