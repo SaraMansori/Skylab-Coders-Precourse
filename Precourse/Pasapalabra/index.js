0e661b2dfb61ba03394f85ed58e561f91546b753
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-19 23:51:00 +0200

    game finished. pending refactoring dry

--- Precourse/Pasapalabra/index.js
+++ Precourse/Pasapalabra/index.js
@@ -2,9 +2,11 @@
 let points = 0;
 let rounds = 0;
 let errors = 0;
-let ranking = true;
+let finishedAllGame = true;
 let finished = false;
 let thisGameQuestions = [];
+let unansweredQuestions = [];
+
 const alphabet = [
     "A",
     "B",
@@ -69,62 +71,110 @@ const normalize = (word) => {
     return normalizedWord;
 };
 
-//Siguiente ronda si hay preguntas en las que se ha pasado palabra
+const nextTurn = () => {
+    for (let i = 0; i < unansweredQuestions.length; i++) {
+        let userAnswer = prompt(unansweredQuestions[i].question);
+        let correctAnswer = normalize(unansweredQuestions[i].answer);
+        if (userAnswer !== null) {
+            userAnswer = normalize(userAnswer);
+        }
+        switch (userAnswer) {
+            case null:
+            case "END":
+                finished = true;
+                finishedAllGame = false;
+                break;
+            case "pasapalabra":
+            case "":
+                console.log("Pasapalabra!");
+                rounds++;
+                break;
+            default:
+                if (userAnswer !== correctAnswer) {
+                    console.log(
+                        `Incorrecto! La respuesta correcta es ${unansweredQuestions[i].answer}`
+                    );
+                    unansweredQuestions[i].status = 2;
+                    errors++;
+                    rounds++;
+                } else if (userAnswer === correctAnswer) {
+                    console.log("Correcto!");
+                    unansweredQuestions[i].status = 1;
+                    points++;
+                    rounds++;
+                }
+        }
+    }
+    unansweredQuestions = unansweredQuestions.filter(
+        (question) => question.status === 0
+    );
+};
 
 const pasapalabraGame = () => {
-    if (window.confirm("Preparado para empezar?")) {
-        for (let i = 0; i < thisGameQuestions.length && !finished; i++) {
-            let userAnswer = prompt(thisGameQuestions[i].question);
-            let correctAnswer = normalize(thisGameQuestions[i].answer);
-            if (userAnswer !== null) {
-                userAnswer = normalize(userAnswer);
-            }
-            switch (userAnswer) {
-                case null:
-                case "END":
-                    finished = true;
-                    ranking = false;
-                    break;
-                case "pasapalabra":
-                case "":
-                    console.log("Pasapalabra!");
+    for (let i = 0; i < thisGameQuestions.length && !finished; i++) {
+        let userAnswer = prompt(thisGameQuestions[i].question);
+        let correctAnswer = normalize(thisGameQuestions[i].answer);
+        if (userAnswer !== null) {
+            userAnswer = normalize(userAnswer);
+        }
+        switch (userAnswer) {
+            case null:
+            case "END":
+                finished = true;
+                finishedAllGame = false;
+                break;
+            case "pasapalabra":
+            case "":
+                console.log("Pasapalabra!");
+                rounds++;
+                break;
+            default:
+                if (userAnswer !== correctAnswer) {
+                    console.log(
+                        `Incorrecto! La respuesta correcta es ${thisGameQuestions[i].answer}`
+                    );
+                    thisGameQuestions[i].status = 2;
+                    errors++;
                     rounds++;
-                    break;
-                default:
-                    if (userAnswer !== correctAnswer) {
-                        console.log(
-                            `Incorrecto! La respuesta correcta es ${thisGameQuestions[i].answer}`
-                        );
-                        thisGameQuestions[i].status = 2;
-                        errors++;
-                        rounds++;
-                    } else if (userAnswer === correctAnswer) {
-                        console.log("Correcto!");
-                        thisGameQuestions[i].status = 1;
-                        points++;
-                        rounds++;
-                    }
-            }
+                } else if (userAnswer === correctAnswer) {
+                    console.log("Correcto!");
+                    thisGameQuestions[i].status = 1;
+                    points++;
+                    rounds++;
+                }
         }
-        if (ranking) {
-            console.log(
-                `Juego terminado. Has acertado ${points} de las preguntas y has cometido ${errors} errores.`
+    }
+    if (finishedAllGame) {
+        do {
+            alert("Empezamos de nuevo el rosco!");
+            unansweredQuestions = thisGameQuestions.filter(
+                (question) => question.status === 0
             );
-            thisGameQuestions = [];
-            points = 0;
-            rounds = 0;
-            alert("Hasta pronto!");
-        } else {
-            console.log(`Has acertado ${points} preguntas`);
-            thisGameQuestions = [];
-            thisGameQuestions = [];
-            points = 0;
-            rounds = 0;
-            alert("Hasta pronto!");
-        }
+            nextTurn();
+        } while (unansweredQuestions.length > 0);
+        console.log(
+            `Juego terminado. Has acertado ${points} de las preguntas y has cometido ${errors} errores.`
+        );
+        questionsArray = [];
+        points = 0;
+        rounds = 0;
+        alert("Hasta pronto!");
+    } else {
+        console.log(`Has acertado ${points} preguntas`);
+        questionsArray = [];
+        points = 0;
+        rounds = 0;
+        alert("Hasta pronto!");
+    }
+};
+
+//Función que ejecuta el comienzo del juego
+const startGame = () => {
+    if (window.confirm("Preparado para empezar la partida?")) {
+        pasapalabraGame();
     } else {
         alert("Hasta pronto!");
     }
 };
 
-pasapalabraGame();
+startGame();
