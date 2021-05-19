a54bac12c22e10cc539fd413b4a79952e16949c0
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-19 11:19:18 +0200

    questions introduced

---
+++ Precourse/Pasapalabra/index.js
@@ -0,0 +1,165 @@
+//Array de objetos que contiene las preguntas y respuestas de cada letra. El status indica: 0-Sin responder; 1-Correcta; 2-Fallada
+
+const questions = [
+    {
+        letter: "A",
+        question: "Con la A. País de sudamérica del que es originario Maradona",
+        answer: "Argentina",
+        status: 0,
+    },
+    {
+        letter: "B",
+        question:
+            "Con la B. Ciudad en la que comenzó a impartirse el mejor bootcamp del mundo",
+        answer: "Barcelona",
+        status: 0,
+    },
+    {
+        letter: "C",
+        question: "Con la C. Entre el número 4 y el número 6",
+        answer: "Cinco",
+        status: 0,
+    },
+    {
+        letter: "D",
+        question:
+            "Con la D. Ropa especial que sirve para cambiar de aspecto en fiestas como Carnaval",
+        answer: "Disfraz",
+        status: 0,
+    },
+    {
+        letter: "E",
+        question: "Con la E. Moneda oficial de Europa",
+        answer: "Euro",
+        status: 0,
+    },
+    {
+        letter: "F",
+        question: "Con la F. Hueso más largo del cuerpo humano",
+        answer: "Fémur",
+        status: 0,
+    },
+    {
+        letter: "G",
+        question: "Con la G. Software de control de versiones",
+        answer: "Git",
+        status: 0,
+    },
+    {
+        letter: "H",
+        question: "Con la H. Juego de cartas de la empresa Blizzard",
+        answer: "Hearthstone",
+        status: 0,
+    },
+    {
+        letter: "I",
+        question:
+            "Con la I. Película de ciencia ficción espacial dirigida por Cristopher Nolan",
+        answer: "Interestellar",
+        status: 0,
+    },
+    {
+        letter: "J",
+        question: "Con la J. Animal grande, amarillo y marrón de largo cuello",
+        answer: "Jirafa",
+        status: 0,
+    },
+    {
+        letter: "K",
+        question: "Con la K. 1000 gramos es un...",
+        answer: "Kilo",
+        status: 0,
+    },
+    {
+        letter: "L",
+        question: "Con la L. Sinónimo de despacio",
+        answer: "Lento",
+        status: 0,
+    },
+    {
+        letter: "M",
+        question: "Con la M. Capital de España",
+        answer: "Madrid",
+        status: 0,
+    },
+    {
+        letter: "N",
+        question: "Con la N. Nombre de un color y a la vez de una fruta",
+        answer: "Naranja",
+        status: 0,
+    },
+    {
+        letter: "Ñ",
+        question:
+            "Contiene la Ñ. Personas que están en la etapa de la infancia",
+        answer: "Niños",
+        status: 0,
+    },
+    {
+        letter: "O",
+        question: "Con la O. Objeto Volador No Identificado",
+        answer: "OVNI",
+        status: 0,
+    },
+    {
+        letter: "P",
+        question: "Con la P. Juego al que estás jugando ahora mismo",
+        answer: "Pasapalabra",
+        status: 0,
+    },
+    {
+        letter: "Q",
+        question:
+            "Con la Q. Producto lácteo obtenido por maduración de la cuajada de la leche",
+        answer: "Queso",
+        status: 0,
+    },
+    {
+        letter: "R",
+        question: "Con la R. Framework de Javascript",
+        answer: "React",
+        status: 0,
+    },
+    {
+        letter: "S",
+        question:
+            "Con la S. Estrella luminosa, centro del sistema planetario en que está situada la Tierra",
+        answer: "Sol",
+        status: 0,
+    },
+    {
+        letter: "T",
+        question:
+            "Con la T. Película de animación de Pixar protagonizada por Sheriff Woody y Buzz Lightyear",
+        answer: "Toy Story",
+        status: 0,
+    },
+    {
+        letter: "U",
+        question: "Con la U. Sinónimo de cosmos ",
+        answer: "Universo",
+        status: 0,
+    },
+    {
+        letter: "W",
+        question:
+            "Con la W. Heroína del universo DC cuyo nombre real es Diana de Themyscira",
+        answer: "Wonder Woman",
+        status: 0,
+    },
+    {
+        letter: "X",
+        question:
+            "Contiene la X. Instrumento musical de viento, de metal, con boquilla de madera, caña y varias llaves, que es de invención moderna y muy usado, principalmente en las bandas militares y orquestas de jazz.",
+        answer: "Saxofón",
+        status: 0,
+    },
+    { letter: "Y", question: "Con la Y. ", answer: "", status: 0 },
+    {
+        letter: "Z",
+        question:
+            "Con la Z. Película de comedia dirigida por Ruben Fleischer ambientada en un apocalipsis zombie",
+        answer: "Zombieland",
+        status: 0,
+    },
+];
